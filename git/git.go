68b6266e409543bec8568a70b6739cb6ecc1f30f
Aaron Boodman <aaron@aaronboodman.com>
2019-02-08 22:18:38 -0800

    Pull cached repo if out of sync

--- git/git.go
+++ git/git.go
@@ -13,19 +13,33 @@ import (
 
 // Clone fetches the latest copy of the git repo at a URL to a local directory.
 func Clone(url *url.URL, dataPath string) (rootPath string, err error) {
+	w := func(err error) error {
+		return errors.Wrap(err, "Could not clone/sync git repo")
+	}
 	// TODO: would be nice to use something more readable than sha1
 	// couldn't find a handy path escaping function
 	dirName := sha1.Sum([]byte(url.String()))
 	fullPath := path.Join(dataPath, string(hex.EncodeToString(dirName[:])))
 	_, err = os.Stat(fullPath)
 
-	if err != nil && !os.IsNotExist(err) {
-		return "", errors.Wrapf(err, "Could not clone into directory: %s", fullPath)
+	if err == nil {
+		// repo already exists, sync it.
+		var wt *gogit.Worktree
+		r, err := gogit.PlainOpen(fullPath)
+		if err == nil {
+			wt, err = r.Worktree()
+			if err == nil {
+				err = wt.Pull(&(gogit.PullOptions{}))
+			}
+		}
+		if err != nil && err != gogit.NoErrAlreadyUpToDate {
+			return "", w(err)
+		}
+		return fullPath, nil
 	}
 
-	if err == nil {
-		// TODO: sync
-		return fullPath, nil
+	if !os.IsNotExist(err) {
+		return "", w(err)
 	}
 
 	_, err = gogit.PlainClone(fullPath, false, &gogit.CloneOptions{
